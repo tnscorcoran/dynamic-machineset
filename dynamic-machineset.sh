1d8da172a71935b62c92d2e65807e85d2cfe2b57
Tom Corcoran <tnscorcoran@gmail.com>
2024-11-03 09:53:46 +1100

    Setup

--- dynamic-machineset.sh
+++ dynamic-machineset.sh
@@ -18,14 +18,14 @@ infrastructure_id=$2
 instanceType=$3
 region=$4
 availabilityZone=$5
-ami=$6
+
 
 echo Input file $input_file
 echo infrastructure_id $infrastructure_id
 echo instanceType $instanceType
 echo region $region
 echo availabilityZone $availabilityZone
-echo ami $ami
+
 
 
 # Check if input file exists
@@ -38,6 +38,6 @@ fi
 
 # Perform the replacement
 
-sed -i.bak -e "s/your_infrastructure_id/$infrastructure_id/g" -e "s/your_instanceType/$instanceType/g" -e "s/your_region/$region/g" -e "s/your_availabilityZone/$availabilityZone/g" -e "s/your_ami/$ami/g" "$input_file"
+sed -i.bak -e "s/your_infrastructure_id/$infrastructure_id/g" -e "s/your_instanceType/$instanceType/g" -e "s/your_region/$region/g" -e "s/your_availabilityZone/$availabilityZone/g" "$input_file"
 
 echo "Replacement complete. Check $input_file for the updated content."
